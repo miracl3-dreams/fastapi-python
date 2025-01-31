50fb59eee4c4edb8665febdcddf9c404ec2952e9
Danielle <daniell07720@gmail.com>
2025-01-31 17:06:33 +0800

    feat: add admin table

--- api/models/admin_model.py
+++ api/models/admin_model.py
@@ -1,14 +1,10 @@
+# models/admin_model.py
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql import func
-
-Base = declarative_base()
+from api.utils.database import Base
 
 class Admin(Base):
-    __tablename__ = 'admins'
+    __tablename__ = "admins"
 
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String(255), nullable=False)
-    last_name = Column(String(255), nullable=False)
-    username = Column(String(255), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)  
