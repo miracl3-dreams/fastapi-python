cc8c98c7e1e84ab1082f225381d4c3f233d7dac6
Danielle <daniell07720@gmail.com>
2025-01-31 17:12:11 +0800

    feat: add user schema

---
+++ api/schemas/user_schema.py
@@ -0,0 +1,14 @@
+# schemas/user_schema.py
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: str
+    is_active: bool
+
+    class Config:
+        from_attributes = True  
