0f13206662758ae0abecf74da74b3f8a5f23bc22
Danielle <daniell07720@gmail.com>
2025-01-31 17:12:44 +0800

    feat: add service admin

--- api/services/admin_service.py
+++ api/services/admin_service.py
@@ -1,13 +1,15 @@
-# admin_service.py
-from api.repositories.admin_repository import AdminRepository
-from api.database import get_database_connection
+# services/admin_service.py
+from api.repositories.admin_repository import create_admin, get_admin_by_username
+from api.schemas.admin_schema import AdminCreate
+from api.utils.encryption import hash_password, verify_password
 
-class AdminService:
-    def __init__(self):
-        # Get the database session here and pass it to AdminRepository
-        self.db_session = get_database_connection()  # Adjust based on your implementation
-        self.admin_repository = AdminRepository(self.db_session)
+async def create_new_admin(db, admin_data: AdminCreate):
+    hashed_password = hash_password(admin_data.password)
+    admin_data.password = hashed_password
+    return await create_admin(db, admin_data)
 
-    async def create_admin(self) -> str:
-        message = await self.admin_repository.create_admin()
-        return message
+async def authenticate_admin(db, username: str, password: str):
+    admin = get_admin_by_username(db, username)
+    if admin and verify_password(password, admin.password): 
+        return admin
+    return None
