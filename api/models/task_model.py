672a8af86c73810a6555effa02f0c5206759ebef
Danielle <daniell07720@gmail.com>
2025-02-05 17:59:32 +0800

    bug: i'll fix tomorrow

---
+++ api/models/task_model.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Boolean, Text
+from api.utils.database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    task_name = Column(String(255), unique=True, nullable=False)
+    task_description = Column(Text, nullable=True)  
+    status = Column(Boolean, nullable=False, default=False) 
