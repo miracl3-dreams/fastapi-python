e050078405c9830af1834e8ce91363fb9dcdf331
Danielle <daniell07720@gmail.com>
2025-02-05 17:54:53 +0800

    chore(set up): Initial set up configuration

--- api/main.py
+++ api/main.py
@@ -2,22 +2,26 @@ from fastapi import FastAPI
 from api.config.config import config
 from fastapi.middleware.cors import CORSMiddleware
 from api.utils.app_response import AppResponse
-from api.routes.v1.admin_router import admin_router
-from api.routes.v1.user_router import user_router
 from api.routes import router as api_router
 from api.utils.database import engine, Base
+from contextlib import asynccontextmanager
 
-# Determine Swagger visibility based on environment
 docs_url = "/docs" if config["app"]["env"] == "development" else None
 redoc_url = "/redoc" if config["app"]["env"] == "development" else None
 
-# Initialize FastAPI app
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
 app = FastAPI(
     title="FAST API",
     version="1.0.0",
     description="FAST API BY DANIELLE LUNAS",
     docs_url=docs_url,
     redoc_url=redoc_url,
+    lifespan=lifespan,
 )
 
 # Middleware for CORS
@@ -30,11 +34,8 @@ app.add_middleware(
 )
 
 # Include routers
-app.include_router(admin_router)
-app.include_router(user_router)
 app.include_router(api_router)
 
-# Root Endpoint
 @app.get("/")
 async def root():
     return AppResponse.send_success(
@@ -43,18 +44,10 @@ async def root():
         code=200
     )
 
-# Asynchronous database initialization
-@app.on_event("startup")
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-# Run the server
 if __name__ == "__main__": 
     import uvicorn
-    # Use config["app"]["port"] dynamically
     uvicorn.run(
         "api.main:app",
-        port=int(config["app"]["port"]),  # Pass port from config
-        reload=True,  # Use in development
+        port=int(config["app"]["port"]),
+        reload=True,
     )
