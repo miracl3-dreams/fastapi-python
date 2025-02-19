8e0068c12eb0757fafd164310850006ea9bd36c8
Danielle <daniell07720@gmail.com>
2025-02-19 20:39:54 +0800

    fix: no changes

--- api/routes/v1/admin_router.py
+++ api/routes/v1/admin_router.py
@@ -7,7 +7,6 @@ from api.utils.database import AsyncSessionLocal
 router = APIRouter()
 admin_controller = AdminController()
 
-# Dependency to get the database session
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
@@ -17,5 +16,5 @@ async def create_admin(admin: AdminCreate, db: AsyncSession = Depends(get_db)):
     return await admin_controller.create_admin(admin, db)
 
 @router.post("/login/authenticate/")
-async def login_admin(username: str, password: str, db: AsyncSession = Depends(get_db)):
-    return await admin_controller.login_admin(username, password, db)
+async def login_admin(email: str, password: str, db: AsyncSession = Depends(get_db)):
+    return await admin_controller.login_admin(email, password, db)
