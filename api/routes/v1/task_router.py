7e8de06898935dabd71c75e4c3fc7ba895d947cb
Danielle <daniell07720@gmail.com>
2025-02-19 21:00:09 +0800

    fix: get all the tasks and just tasks id

--- api/routes/v1/task_router.py
+++ api/routes/v1/task_router.py
@@ -1,7 +1,8 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from api.controllers.task_controller import TaskController
-from api.schemas.task_schema import TaskCreate, TaskUpdate, TaskDelete
+from api.schemas.task_schema import TaskCreate, TaskUpdate, TaskDelete, TaskResponse
 from api.utils.database import AsyncSessionLocal
 
 router = APIRouter()
@@ -10,6 +11,14 @@ task_controller = TaskController()
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
+
+@router.get("/get/{task_id}", response_model=TaskResponse)
+async def get_task(task_id: int, db: AsyncSession = Depends(get_db)):
+    return await task_controller.get_task(task_id, db)
+
+@router.get("/get", response_model=List[TaskResponse])
+async def get_all_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_controller.get_all_tasks(db)
 
 @router.post("/create", response_model=TaskCreate)
 async def create_task(task: TaskCreate, db: AsyncSession = Depends(get_db)):
