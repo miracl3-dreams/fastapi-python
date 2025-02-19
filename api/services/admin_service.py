50fd69b0413c1c56dae0be2dab584b85ac13750f
Danielle <daniell07720@gmail.com>
2025-02-19 20:41:10 +0800

    fix: exception

--- api/services/admin_service.py
+++ api/services/admin_service.py
@@ -1,24 +1,27 @@
+from fastapi import HTTPException
 from api.repositories.admin_repository import AdminRepository
 from api.schemas.admin_schema import AdminCreate, AdminResponse
 from api.utils.encryption import hash_password, verify_password
 from api.utils.custom_error import AuthError
+from api.utils.exceptions import EmailAlreadyExistsException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 class AdminService:
-    def __init__(self, admin_repository: AdminRepository = AdminRepository()):
-        self.admin_repository = admin_repository
+    def __init__(self):
+        self.admin_repository = AdminRepository()
 
-    async def create_new_admin(self, db: AsyncSession, admin_data: AdminCreate) -> AdminResponse:
-        # Hash the password before storing
+    async def create_new_admin(self, db: AsyncSession, admin_data: AdminCreate):
         hashed_password = hash_password(admin_data.password)
         admin_data.password = hashed_password
-        # Store the new admin using the repository
-        admin = await self.admin_repository.create_admin(db, admin_data)
-        return AdminResponse.model_validate(admin)  
-    
-    async def authenticate_admin(self, db: AsyncSession, username: str, password: str) -> AdminResponse:
-        # Fetch the admin by username
-        admin = await self.admin_repository.get_admin_by_username(db, username)
+        
+        try:
+            admin = await self.admin_repository.create_admin(db, admin_data)
+        except EmailAlreadyExistsException as e:
+            raise EmailAlreadyExistsException()  
+        return admin  
+        
+    async def authenticate_admin(self, db: AsyncSession, email: str, password: str):
+        admin = await self.admin_repository.get_admin_by_email(db, email)
         if admin and verify_password(password, admin.password):
-            return AdminResponse.model_validate(admin)
-        raise AuthError("Invalid credentials")
+            return admin
+        raise AuthError()
--- api/services/user_service.py
+++ api/services/user_service.py
@@ -1,24 +1,9 @@
-# # services/user_service.py
-# from api.repositories.user_repository import create_user, get_user_by_email
-# from api.schemas.user_schema import UserCreate
-# from api.utils.encryption import hash_password, verify_password
-
-# async def create_new_user(db, user_data: UserCreate):
-#     hashed_password = hash_password(user_data.password)
-#     user_data.password = hashed_password
-#     return await create_user(db, user_data)
-
-# async def authenticate_user(db, email: str, password: str):
-#     user = get_user_by_email(db, email)
-#     if user and verify_password(password, user.password):  # Implement verify_password in encryption.py
-#         return user
-#     return None
-
 from api.repositories.user_repository import UserRepository
 from api.schemas.user_schema import UserCreate
 from api.utils.encryption import hash_password, verify_password
 from api.utils.custom_error import AuthError
 from sqlalchemy.ext.asyncio import AsyncSession
+from api.utils.exceptions import EmailAlreadyExistsException
 
 class UserService:
     def __init__(self):
@@ -27,11 +12,16 @@ class UserService:
     async def create_new_user(self, db: AsyncSession, user_data: UserCreate):
         hashed_password = hash_password(user_data.password)
         user_data.password = hashed_password
-        # Call the repository to store the new user
-        return await self.user_repository.create_user(db, user_data)
+        
+        try:
+            user = await self.user_repository.create_user(db, user_data)
+        except EmailAlreadyExistsException as e:
+            raise EmailAlreadyExistsException()  
+        return user  
+        
 
     async def authenticate_user(self, db: AsyncSession, email: str, password: str):
         user = await self.user_repository.get_user_by_email(db, email)
         if user and verify_password(password, user.password):
             return user
-        raise AuthError("Invalid credentials")
+        raise AuthError()
