4678973f9cfb76fc18067e7129e4d8de88311603
Danielle <daniell07720@gmail.com>
2025-02-19 20:38:49 +0800

    fix: db problem

--- api/repositories/user_repository.py
+++ api/repositories/user_repository.py
@@ -2,9 +2,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from api.models.user_model import User
 from api.schemas.user_schema import UserCreate
+from api.utils.exceptions import EmailAlreadyExistsException
 
 class UserRepository:
     async def create_user(self, db: AsyncSession, user_data: UserCreate):
+        query = select(User).filter(User.email == user_data.email)
+        result = await db.execute(query)
+        existing_user = result.scalars().first()
+        
+        if existing_user:
+            raise EmailAlreadyExistsException()
+
         new_user = User(**user_data.model_dump())
         db.add(new_user)
         await db.commit()
