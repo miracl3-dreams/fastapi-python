49cbda54623377b6d684ad6795a60e9c652c4414
Danielle <daniell07720@gmail.com>
2025-02-19 20:40:40 +0800

    feat: add example

--- api/schemas/task_schema.py
+++ api/schemas/task_schema.py
@@ -5,6 +5,14 @@ class TaskCreate(BaseModel):
     task_description: str
     status: bool
 
+class TaskUpdate(BaseModel):
+    task_name: str
+    task_description: str
+    status: bool
+
+class TaskDelete(BaseModel):
+    id: int
+
 class TaskResponse(BaseModel):
     id: int 
     task_name: str
--- api/schemas/user_schema.py
+++ api/schemas/user_schema.py
@@ -1,14 +1,12 @@
-# schemas/user_schema.py
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr
 
-class UserCreate(BaseModel):
-    email: str
-    password: str
+class UserCreate(BaseModel): 
+    email: EmailStr = Field(..., min_length=3, max_length=50, example="test123@gmail.com")
+    password: str = Field(..., min_length=8, max_length=50, example="test1234")
 
 class UserResponse(BaseModel):
-    id: int
-    email: str
-    is_active: bool
+    id: int 
+    email: EmailStr = Field(..., min_length=3, max_length=50)
 
     class Config:
-        from_attributes = True  
+        from_attributes = True
