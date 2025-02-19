02dcc16752be4e148c7a05793ccd8ac875f2e968
Danielle <daniell07720@gmail.com>
2025-02-19 20:37:48 +0800

    fix: string and adding example

--- api/models/admin_model.py
+++ api/models/admin_model.py
@@ -5,5 +5,5 @@ class Admin(Base):
     __tablename__ = "admins"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
     password = Column(String(255), nullable=False)  
