3f5b5a689b3e0fe116e792dd7f7190aeb6aa498f
Danielle <daniell07720@gmail.com>
2025-02-21 14:35:33 +0800

    fix: user response

--- api/services/user_service.py
+++ api/services/user_service.py
@@ -1,6 +1,6 @@
 from api.repositories.user_repository import UserRepository
-from api.schemas.user_schema import UserCreate
-from api.utils.encryption import hash_password, verify_password
+from api.schemas.user_schema import UserCreate, UserResponse
+from api.utils.password import PasswordManager
 from api.utils.custom_error import AuthError
 from sqlalchemy.ext.asyncio import AsyncSession
 from api.utils.exceptions import EmailAlreadyExistsException
@@ -9,19 +9,22 @@ class UserService:
     def __init__(self):
         self.user_repository = UserRepository()
 
-    async def create_new_user(self, db: AsyncSession, user_data: UserCreate):
-        hashed_password = hash_password(user_data.password)
+    async def create_new_user(self, db: AsyncSession, user_data: UserCreate) -> UserResponse:
+        """Hash the password using PasswordManager"""
+        hashed_password = PasswordManager.hash_password(user_data.password)
         user_data.password = hashed_password
         
+        """Create User"""
         try:
             user = await self.user_repository.create_user(db, user_data)
         except EmailAlreadyExistsException as e:
             raise EmailAlreadyExistsException()  
-        return user  
+        return UserResponse.model_validate(user)
         
 
-    async def authenticate_user(self, db: AsyncSession, email: str, password: str):
+    async def authenticate_user(self, db: AsyncSession, email: str, password: str) -> UserResponse:
+        """Logging User."""
         user = await self.user_repository.get_user_by_email(db, email)
-        if user and verify_password(password, user.password):
-            return user
+        if user and PasswordManager.verify_password(password, user.password):
+            return UserResponse.model_validate(user)
         raise AuthError()
