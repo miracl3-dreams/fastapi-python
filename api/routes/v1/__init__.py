c69bbeb4083717518230a55e596de023b635c204
Danielle <daniell07720@gmail.com>
2025-06-04 02:04:33 +0800

    fix: changing prefix name in rfid

--- api/routes/v1/__init__.py
+++ api/routes/v1/__init__.py
@@ -6,5 +6,5 @@ from .task_auth_router import router as task_auth_router
 router = APIRouter(prefix="/api/v1")
 
 router.include_router(test_router, prefix="/test", tags=["Test"])
-router.include_router(user_rfid_auth_router, prefix="/rfid", tags=["Auth-RFID"])
+router.include_router(user_rfid_auth_router, prefix="/auth-rfid", tags=["Auth-RFID"])
 router.include_router(task_auth_router, prefix="/tasks", tags=["Task"])
