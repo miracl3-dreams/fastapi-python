2c5c84c84ba184581b934d9dc69fcae64e235131
Danielle <daniell07720@gmail.com>
2025-06-04 02:02:42 +0800

    feat: added relationship in user

--- api/models/task_model.py
+++ api/models/task_model.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, Text, String, Boolean
-# from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, Text, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from ..models import Base
 
 class Task(Base):
@@ -7,9 +7,11 @@ class Task(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     task_name = Column(String(255), unique=True, nullable=False)
-    task_description = Column(Text, nullable=True)  
+    task_description = Column(Text, nullable=True)
     status = Column(Boolean, default=False, nullable=False)
-    
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    user = relationship("User", back_populates="tasks")
+
     def status_label(self):
-        """Returns 'incomplete' if False, 'complete' if True."""
         return "complete" if self.status else "incomplete"
